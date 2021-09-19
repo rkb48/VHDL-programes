d4f2c4d87f1c2ceaeee4d984121756c42185f314
Rkb <44485189+rkb48@users.noreply.github.com>
2021-09-19 19:07:30 +0530

    Delay programe

---
+++ D_FF.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:51:58 05/30/2016 
+-- Design Name: 
+-- Module Name:    D_FF - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity D_FF is
+    Port ( Clk : in  STD_LOGIC;
+           D : in  signed (15 downto 0);
+           Q : out signed (15 downto 0));
+end D_FF;
+
+architecture Behavioral of D_FF is
+
+begin
+process(Clk)
+begin
+	if(rising_edge(Clk)) then
+		Q<=D;
+	end if;
+end process;
+end Behavioral;
+
